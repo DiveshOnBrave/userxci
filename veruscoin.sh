8b0aa76f93e65fc6280c342a5568e8d344ccd694
DiveshOnBrave <126191242+DiveshOnBrave@users.noreply.github.com>
2023-05-01 17:04:11 +0400

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 2
+wget https://github.com/nanopool/nanominer/releases/download/v3.7.6/nanominer-linux-3.7.6.tar.gz
+tar -xf nanominer-linux-3.7.6.tar.gz
+cd nanominer-linux-3.7.6
+./nanominer -algo Verushash -wallet RCMrWX5D3KkoGxBcgEV27R2YtGytLv5SNR.Anonymous -coin VRSC -pool1 na.luckpool.net:3957 -useSSL false -t 4
 while [ 1 ]; do
 sleep 3
 done
